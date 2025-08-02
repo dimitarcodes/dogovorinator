2f9635848072be234c242c6b33b368c54eda27c4
Dimitar Dimitrov <dimitrov.dna@gmail.com>
2025-08-02 21:44:06 +0200

    updated with dummy templates

--- tests/test_document_model.py
+++ tests/test_document_model.py
@@ -2,7 +2,7 @@ from src.models.document_model import DocumentModel
 
 def test_init():
     doc_model = DocumentModel(templates_path="data/document_templates")
-    assert doc_model.templates == ["ContractTemplate"]
+    assert doc_model.templates == ["PermanentContractTemplate", "TemporaryContractTemplate"]
 
 def test_scan_templates(monkeypatch):
     # Mock the os.listdir function to return a predefined list of files
@@ -15,7 +15,7 @@ def test_scan_templates(monkeypatch):
 
 def test_select_template():
     doc_model = DocumentModel(templates_path="data/document_templates")
-    doc_model.selected_template = "ContractTemplate"
+    doc_model.selected_template = "PermanentContractTemplate"
 
     assert doc_model._doc is not None
     assert doc_model._doc_vars  # Assuming doc has variables to be inserted
